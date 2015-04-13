ea48da755e1734abf77ee1c00b7d3254b4bb4084
oncletom <hi@oncletom.io>
2015-04-14 00:22:20 +0100

    Get everybody on board.

--- bin/get-attendees.js
+++ bin/get-attendees.js
@@ -5,30 +5,47 @@ var path = require('path');
 var request = require('superagent');
 var md5 = require('MD5');
 
+var maxPerPage = 20;
+
+if (!process.env.ACCESS_TOKEN || !process.env.API_KEY) {
+  console.log('ACCESS_TOKEN or API_KEY are missing. Skipping this step…');
+  process.exit(0);
+}
+
 var stream = fs.createWriteStream(path.join(__dirname, '..', '.attendees.json'));
-stream.write('[');
+stream.write('[', paginate.bind(null, 1));
 
-request
-  .get('https://api.weezevent.com/participants')
-  .query({
-    access_token: process.env.ACCESS_TOKEN,
-    api_key: process.env.API_KEY,
-    'id_event[]': process.env.ID_EVENT,
-    full: true,
-    max: 20,
-    page: 1
-  })
-  .end(function(err, res){
-    res.body.participants.forEach(function(attendee){
-      stream.write(JSON.stringify({
-        name: attendee.owner.first_name + ' ' + attendee.owner.last_name,
-        gravatar: 'https://secure.gravatar.com/avatar/' + md5(attendee.owner.email) + '.jpg?s=75&amp;r=g',
-        twitter: filterTwitterUsernameFromAnswers(attendee.answers)
-      }) + ', ');
+function paginate(page) {
+  request
+    .get('https://api.weezevent.com/participants')
+    .query({
+      access_token: process.env.ACCESS_TOKEN,
+      api_key:      process.env.API_KEY,
+      'id_event[]': process.env.ID_EVENT,
+      full:         true,
+      max:          maxPerPage,
+      page:         page
+    })
+    .end(function (err, res) {
+      console.log('Receiving page #%d results…', page);
+
+      res.body.participants.forEach(function (attendee) {
+        stream.write(JSON.stringify({
+          name:     attendee.owner.first_name + ' ' + attendee.owner.last_name,
+          gravatar: 'https://secure.gravatar.com/avatar/' + md5(attendee.owner.email) + '.jpg?s=75&amp;r=g',
+          twitter:  filterTwitterUsernameFromAnswers(attendee.answers)
+        }) + ', ');
+      });
+
+
+      if (res.body.participants.length < maxPerPage) {
+        return stream.write('{}]');
+      }
+      else {
+        return paginate(page + 1);
+      }
     });
-
-    stream.write('{}]');
-  });
+}
 
 
 function filterTwitterUsernameFromAnswers(answers){
--- js/attendees.jsx
+++ js/attendees.jsx
@@ -1,7 +1,12 @@
 'use strict';
 
 var React = require('react');
-var attendees = require('../.attendees.json');
+var attendees = require('../.attendees.json')
+  .filter(function(person, i, arr){
+    return person.name && arr.slice(i+1, arr.length).some(function(p){
+      return p.name !== person.name;
+    });
+  });
 
 module.exports = React.createClass({
   getDefaultProps: function(){
@@ -9,19 +14,17 @@ module.exports = React.createClass({
   },
 
   render: function(){
-    return <ol>{this.renderAttendees()}</ol>;
+    return (<ol>{this.renderAttendees()}</ol>);
   },
 
   renderAttendees: function (){
     return this.props.attendees.map(function(person, i){
-      return (<li className="column attendee" key={i}>
+      return (<li className="column person" key={i}>
         <p className="avatar">
-          <img src={person.gravatar} alt=""/>
+          <img src={person.gravatar + '&d=http://sudweb.fr/2015/img/you-re-awesome.jpg'} alt=""/>
         </p>
 
-        <h3>{person.name}</h3>
-
-        {this.renderTwitterHandle(person)}
+        <h3>{person.twitter ? this.renderTwitterHandle(person) : person.name}</h3>
       </li>);
     }, this);
   },
@@ -31,6 +34,6 @@ module.exports = React.createClass({
       return null;
     }
 
-    return (<h4><a href={'https://twitter.com/' + person.twitter}>@{person.twitter}</a></h4>);
+    return (<a href={'https://twitter.com/' + person.twitter} title={person.name}>@{person.twitter}</a>);
   }
 });
